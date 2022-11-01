76ac139d3a91f5d193f88e33d1d434a3dc84f3d0
Satendra Raj <satendra.dev124@gmail.com>
2022-11-01 14:32:09 +0530

    edit properties and add layer

--- src/share_screen/share_screen.jsx
+++ src/share_screen/share_screen.jsx
@@ -1,10 +1,11 @@
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, updateDoc } from "firebase/firestore";
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import React, { useEffect, useRef, useState } from "react";
 import { db } from "../firebase";
 import { CircularProgress } from "@mui/material";
 import { Marker, Popup } from "mapbox-gl";
 import Update from "./Update";
+import { async } from "@firebase/util";
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2F0ZW5kcmExMjQxIiwiYSI6ImNreGc2MjI5cjFwaTQyd3BkeGZ6NWVhMHUifQ.Wh1LgnYc3GQFGCJ7l-C2tQ';
 
@@ -15,11 +16,18 @@ const SharePage = () => {
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
   const [isUploading,setUploading] = useState(false);
-
+  const [visible,setVisible] = useState(false);
+  const [key,setKey] = useState('');
+  const [value,setValue] = useState('');
+  const [curIdx,setCurIdx] = useState(0);
+  // const [curFeature,setCurFeature] = useState(null);
   let path = window.location.href;
   let paths = path.split('/');
-  let id = paths[paths.length - 1].split('?')[0];
+  const prefix = paths[paths.length - 1].split('?');
+  let id = prefix[0];
+  
   const ref = doc(db, 'data', id);
+ 
   const [data, setData] = useState(null);
   const load_data = async () => {
     const document = await getDoc(ref);
@@ -30,6 +38,38 @@ const SharePage = () => {
       });
     // setLat(document.data().features[0].geometry.coordinates[0]);
     // setLng(document.data().features[0].geometry.coordinates[1]);
+  }
+  const update_properties = async ()=>{
+    setUploading(true);
+    // console.log(data['features'][curIdx],curIdx);
+    data['features'][curIdx]['properties'][key] = value;
+    await updateDoc(ref,data);
+    setData(data);
+    setKey('');
+    setValue('');
+    setUploading(false);
+    // console.log(data);
+  }
+
+  const add_layer = async (e)=>{
+    console.log(e);
+    var GetFile = new FileReader();
+    GetFile.onload = async () => {
+        var newdata = GetFile.result;
+        console.log(newdata)
+        newdata = JSON.parse(newdata)
+        setUploading(true);
+        let totalData = data;
+        totalData.features = [
+          ...totalData.features,
+          ...newdata.features
+        ]
+        const s = await updateDoc(ref,totalData);
+        setUploading(false);
+        window.location.reload(false);
+        // console.log("DONE",s);
+    }
+    GetFile.readAsText(e.target.files[0]);
   }
 
   const initMap = () => {
@@ -59,10 +99,16 @@ const SharePage = () => {
 
   `;
   const addMarkers = (data)=>{
-    for(const points of data.features){
+    let i = 0;
+    for(let i = 0;i<data.features.length;i++){
+      let points = data.features[i];
       console.log(points);
-      const marker = new Marker().setLngLat(points.geometry.coordinates).addTo(map.current)
-      .setPopup(new Popup().setHTML(html));
+      const marker = new Marker().setLngLat(points.geometry.coordinates).addTo(map.current);
+        marker.getElement().addEventListener('click', () => {
+        setVisible(true);
+        // setCurFeature(points.properties);
+        setCurIdx(i);
+      });
     }
   }
 
@@ -102,17 +148,59 @@ const SharePage = () => {
     }
     GetFile.readAsText(e.target.files[0]);
   }
+  let curFeature = data? data.features[curIdx].properties:null;
   return (
     <div>
       {
           <div>
-            <div style={{position:'absolute',zIndex:1000,backgroundColor:'white',borderRadius:10,padding:'10px'}}>
+            {isUploading?(<div style={
+              {
+                position:'absolute',
+                top: '50vh',
+                right:'50vw',
+                zIndex:1000,
+              }
+              }>
+              <CircularProgress/>
+            </div>):null}
+            <div style={{position:'absolute',zIndex:10,backgroundColor:'white',borderRadius:10,padding:'10px'}}>
               <button onClick={exportGeoJson}>Export as geojson</button>
               <br/>
                Update using Geojson File - 
               <input type={'file'} onChange={updateGeoJson}  name={'Update with geojson'} placeholder={'choose geojson file'}></input>
-              {isUploading?<CircularProgress/>:null}
+              <br/>
+              Add new Layer
+              <input type={'file'} onChange={add_layer}  name={'add layer with geojson'} placeholder={'choose geojson file'}></input>
+              {/* {isUploading?<CircularProgress/>:null} */}
             </div>
+            {
+              visible?(
+                <div style={{position:'absolute',zIndex:1000,backgroundColor:'white',borderRadius:10,padding:'10px',right:0}}>
+                   <button onClick={()=>{setVisible(false)}}>x</button>
+                  {
+                    curFeature?
+                    Object.keys(curFeature).map((feature)=>{
+                      return <div>{feature} : {curFeature[feature]}</div>
+                    })
+                    :null
+                    (()=>{
+                      if(!curFeature) return null;
+                      let html = '';
+                      for(const feature of Object.keys(curFeature)){
+                        html += feature + ':' + curFeature[feature];
+                      }
+                      console.log(html);
+                      return html;
+                    })()
+                  }
+                  <div>
+                    <input type='text' placeholder="Key" onChange={(e)=>(setKey(e.target.value))} value={key}/>:<input type='text' placeholder="Value" onChange={(e)=>(setValue(e.target.value))} value={value}/><button onClick={update_properties}>+</button>
+                  </div>
+                  {/* {isUploading?<CircularProgress/>:null} */}
+                 
+                </div>
+              ):null
+            }
             <div ref={mapContainer} className="map-container" style={{ height: "100vh" }} />
           </div>
       }
